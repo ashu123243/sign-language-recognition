7a40990506c41e5dd8cf4210802abade009c1b10
ashu123243 <ashutosh.24219@knit.ac.in>
2026-08-16 13:40:10 +0000

    update constants.py

--- src/sign_language_detection/constant/constants.py
+++ src/sign_language_detection/constant/constants.py
@@ -4,7 +4,7 @@ from pathlib import Path
 # Project Root
 # ==============================
 
-PROJECT_ROOT=Path(__file__).resolve().parent[3]
+PROJECT_ROOT=Path(__file__).resolve().parents[3]
 
 # ==============================
 # Data Directories
